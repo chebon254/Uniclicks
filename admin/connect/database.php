c1de7224f597f4bc9e55e7dedf69cf304afb577f
chebon254 <kelvinchebon90@gmail.com>
2024-07-03 15:10:35 +0300

    Test Success

--- admin/connect/database.php
+++ admin/connect/database.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require '../../vendor/autoload.php'; // Ensure Composer autoload is included
+// require '../../vendor/autoload.php'; // Ensure Composer autoload is included
 
 // Database connection details
 $servername = "localhost";
